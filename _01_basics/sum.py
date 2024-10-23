d4ef2196501dc672ce89ff99a02adeac3c5d3477
rop2024 <168510129+rop2024@users.noreply.github.com>
2024-10-23 04:32:46 +0000

    used input()

---
+++ _01_basics/sum.py
@@ -0,0 +1,11 @@
+# program to take input from the user and add them
+
+def main():
+    x = int(input("Enter a number: "))
+    y = int(input("Enter second number: "))
+
+    print(f"sum:{x+y}")
+
+
+if __name__ == "__main__":
+    main()
